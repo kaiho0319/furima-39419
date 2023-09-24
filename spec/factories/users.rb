e72c61ddf5de7bcefd8bf55e3182bf2eb686ab1d
kaiho0319 <k7a8i9h20o@gmail.com>
2023-09-24 18:01:32 +0900

    issue修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,9 @@
 FactoryBot.define do
   factory :user do
-    # nickname              {Faker::Name.initials(number: 2)}
-    # email                 {Faker::Internet.free_email}
-    # password              {Faker::Internet.password(min_length: 6)}
-    # password_confirmation {password}
-    nickname { 'testuser' }
-    email { 'test@example.com' }
-    password { 'aaa1234' }
-    password_confirmation { 'aaa1234' }
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
     name { '海保' }
     last_name { '徹' }
     name_in_katakana { 'カイホ' }
