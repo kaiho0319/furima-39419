7400b81a3a93f3e7243c24a5f7620b8365acaa64
kaiho0319 <k7a8i9h20o@gmail.com>
2023-09-27 20:23:18 +0900

    ActiveHash実装

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 
+gem 'active_hash'
 
 group :production do
   gem 'pg'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  has_one :purchasse
+
+  validatas :
+  validates :category_information_id_id, numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_information_id
+  belongs_to :product_condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_days
+  belongs_to :region_of_origin
+
+
 end
