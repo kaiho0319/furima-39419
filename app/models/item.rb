87b71c7ce9a6f578b39e85428ecf58a53693fa53
kaiho0319 <k7a8i9h20o@gmail.com>
2023-09-30 10:19:11 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,14 @@ class Item < ApplicationRecord
   has_one :purchasse
 
   validatas :
-  validates :category_information_id_id, numericality: { other_than: 1 }
+  validatas :product_name, presence: true
+  validatas :product_description, presence: true
+  validates :category_information_id_id, presence: true, numericality: { other_than: 1 }
+  validates :product_condition_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_days_id, presence: true, numericality: { other_than: 1 }
+  validates :region_of_origin_id, presence: true, numericality: { other_than: 1 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category_information_id
