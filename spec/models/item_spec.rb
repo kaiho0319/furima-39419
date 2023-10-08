0ce35d8bcc478e7ba020aa6066ebefa9325b92eb
kaiho0319 <k7a8i9h20o@gmail.com>
2023-10-08 17:13:42 +0900

    テストコード記載

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,8 +13,9 @@ RSpec.describe Item, type: :model do
     end
     context '商品が出品できない場合' do
       it 'imageが空では保存できない' do
-        @item.image = ''
-        expect(@item).to be_valid
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'product_nameが空では保存できない' do
         @item.product_name = ''
