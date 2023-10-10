6e34ecc82ad3c0abfe15cdf2ca3de8a97493f7b3
kaiho0319 <k7a8i9h20o@gmail.com>
2023-10-10 23:07:47 +0900

    商品情報編集ページの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,12 @@ def edit
 end
 
 def update
-  item = Item.find(params[:id])
-  item.update(item_params)
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render :edit
+  end
 end
 
 
