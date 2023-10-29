163ab611b33810df82908fe86d8b8dd5f5ea546f
kaiho0319 <k7a8i9h20o@gmail.com>
2023-10-29 19:22:32 +0900

    migration修正

--- db/migrate/20231023134942_rename_old_column_name_to_new_column_name.rb
+++
@@ -1,6 +0,0 @@
-class RenameOldColumnNameToNewColumnName < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :orders, :users_id, :user_id
-    rename_column :orders, :items_id, :item_id
-  end
-end
--- db/migrate/20231023144238_change_column_name_in_items.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNameInItems < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :addresses, :orders_id, :order_id
-  end
-end
