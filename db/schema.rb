56085e51de7074d71a8a847533f1a7f8a36d3395
kaiho0319 <k7a8i9h20o@gmail.com>
2023-10-29 20:39:19 +0900

    userモデル修正

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2023_10_23_144238) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
     t.string "name", null: false
     t.string "last_name", null: false
